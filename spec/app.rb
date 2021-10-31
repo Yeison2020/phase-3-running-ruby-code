57050e314aa91c09e879be108eda30119fd80a06
Yeison <enmanuelquiterios@gmail.com>
2021-10-30 22:36:36 -0400

    First Ruby Code

---
+++ spec/app.rb
@@ -0,0 +1,10 @@
+puts 'Hello World'
+
+puts('Hello Friends')
+
+print "Hello Dominicans \n"
+
+p [1,2,3,4,5]
+
+
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
